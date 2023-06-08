e7d016f6f3557bef7be3d45cba02c5762d96c51e
Myungsuk Oh <oharon@naver.com>
2023-06-08 10:19:30 +0200

    fast.py v8

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open('requirements.txt') as f:
     content = f.readlines()
 requirements = [x.strip() for x in content if 'git+' not in x]
 
-setup(name='sd_package',
+setup(name='suicidality_detector',
       version="1.0",
-      description="Project Description",
+      description="Project suicidality_detector",
       packages=find_packages(),
       install_requires=requirements,
       test_suite='tests',
       # include_package_data: to install data from MANIFEST.in
-      include_package_data=True,
-      scripts=['scripts/sd_package-run'],
+      # include_package_data=True,
+      #scripts=['scripts/sd_package-run'],
       zip_safe=False)
