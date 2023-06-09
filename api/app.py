473550f31785fd880c972759ec441e3bbcedb0b4
Galina Orlova <galiorlov@gmail.com>
2023-06-09 16:48:01 +0200

    updated random possibilities

--- api/app.py
+++ api/app.py
@@ -1,68 +1,65 @@
+
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-from fast import predict
-
-
-
+import os
+import sys
+sys.path.append(os.getcwd())
+import random
+# from fast import predicts
 # color sequence
 color_sequence = ['#AFEEEE', '#48D1CC', '#40E0D0', '#00CED1', '#20B2AA']
 # text input cell for users
 user_input = st.text_input("Enter Text:")
-
 # button uses the fast_predict function to get a prediction
 if st.button("Predict"):
-    response = predict(user_input)
-
+    # response = predict(user_input)
+    ## dummy input for test ##
+    probabilities = [round(random.uniform(0.01, 0.99), 2) for i in range(0,5)]
+    labels = [0, 1, 2, 3, 4]
+    response = {label: prob for label, prob in zip(labels, probabilities)}
+    ## dummy input for test ##
     max_val_p = max(response.values())
-
     for labels, probabilities in response.items():
         if probabilities == max_val_p:
             max_val = labels
-
     def classifier(max_val):
         if max_val == 4:
-            return {'max_val' : 'Supportive', 'probability' : max_val_p, 'explanation' : 'description on Supportive'}
-        elif max_val == 3:
-            return {'max_val' : 'Indicator', 'probability' : max_val_p, 'explanation' : 'description on Indicator'}
+            return {'category' : 'Supportive', 'probability' : max_val_p, 'explanation' : 'The author of this text shows empathy toward another person regarding suicidality and offers help or advice'}
         elif max_val == 2:
-            return {'max_val' : 'Ideation', 'probability' : max_val_p, 'explanation' : 'description on Ideation'}
+            return {'max_val' : 'Ideation', 'probability' : max_val_p, 'explanation' : 'This text contains thoughts about suicide, ranging from passing considerations to detailed plans'}
         elif max_val == 1:
-            return {'max_val' : 'Behavior', 'probability' : max_val_p, 'explanation' : 'description on Behavior'}
+            return {'max_val' : 'Behavior', 'probability' : max_val_p, 'explanation' : 'This text reports actions that are related to suicide but do not constitute an actual attempt, such as preparing to commit suicide.'}
         elif max_val == 0:
-            return {'max_val' : 'Attempt', 'probability' : max_val_p, 'explanation' : 'description on Attempt'}
+            return {'max_val' : 'Attempt', 'probability' : max_val_p, 'explanation' : 'This text reports a suicide attempt, that is, a specific act with the intention of dying in the process.'}
+        elif max_val == 3:
+            return {'max_val' : 'Indicator', 'probability' : max_val_p, 'explanation' : 'This text indicates a potential suicide risk without specifically mentioning suicide.'}
         else:
             return {'max_val' : 'error', 'probability' : 'error', 'explanation' : 'error'}
-
-
-
-
-
-
     prediction = classifier(max_val)
-    print(prediction)
-
-    # result as the output class
-    if max_val < 4:
-        st.markdown(f"<h2 style='text-align: center; color: red;'>Suicidal {prediction['max_val']}</h2>", unsafe_allow_html=True)
-    else:
-        st.markdown(f"<h2 style='text-align: center; color: red;'>{prediction['max_val']}</h2>", unsafe_allow_html=True)
-
-    # short interpretation of the prediction
+    st.markdown(f"This assessment is based on an AI analysis and cannot replace a psychiatric/psychological evaluation!")
+    st.markdown(f"<h2 style='text-align: center; color: red;'>{prediction['max_val']}</h2>", unsafe_allow_html=True)
     st.markdown(f"## Interpretation: ")
     st.markdown(f"<div style='font-size: 16px;'>{prediction['explanation']}</div>", unsafe_allow_html=True)
-
-    # Show the scores for each class as a fancy bar chart
-    graph_df = pd.DataFrame.from_dict(prediction, orient='index').reset_index()
-    graph_df.columns=['Category', 'probability']
-    fig = px.bar(graph_df, y='Category', x='probability', color='Category', orientation='v',
-                 text='probability', title='Category probability', color_discrete_sequence=color_sequence)
-    fig.update_layout(xaxis_title="probability", yaxis_title="Category", template='plotly_white', autosize=True)
-    st.plotly_chart(fig)
-
-    # Display the recommendation area, with placeholder text for now
-    st.subheader("Recommendation:")
-    st.markdown("<div style='font-size: 16px;'>Clara's recommendation text ... .</div>", unsafe_allow_html=True)
-
-# Display the disclaimer
-st.markdown("<div style='font-size: 14px; color: gray;'>DISCLAIMER: Making wrong predictions could lead to severe consequences. This application is no replacement for professionals, but rather supports psychiatric care.</div>", unsafe_allow_html=True)
+    # result as the output class
+#     if max_val < 4:
+#         st.markdown(f"<h2 style='text-align: center; color: red;'>Suicidal {prediction['max_val']}</h2>", unsafe_allow_html=True)
+#     else:
+#         st.markdown(f"<h2 style='text-align: center; color: red;'>{prediction['max_val']}</h2>", unsafe_allow_html=True)
+#     # short interpretation of the prediction
+#     st.markdown(f"## Interpretation: ")
+#     st.markdown(f"<div style='font-size: 16px;'>{prediction['explanation']}</div>", unsafe_allow_html=True)
+#     # Show the scores for each class as a fancy bar chart
+#     graph_df = pd.DataFrame.from_dict(prediction, orient='index').reset_index()
+#     graph_df.columns=['Category', 'probability']
+#     fig = px.bar(graph_df, y='Category', x='probability', color='Category', orientation='v',
+#                  text='probability', title='Category probability', color_discrete_sequence=color_sequence)
+#     fig.update_layout(xaxis_title="probability", yaxis_title="Category", template='plotly_white', autosize=True)
+#     st.plotly_chart(fig)
+#     # Display the recommendation area, with placeholder text for now
+#     st.subheader("Recommendation:")
+#     st.markdown("<div style='font-size: 16px;'>Clara's recommendation text ... .</div>", unsafe_allow_html=True)
+# # Display the disclaimer
+# st.markdown("<div style='font-size: 14px; color: gray;'>DISCLAIMER: Making wrong predictions could lead to severe consequences. This application is no replacement for professionals, but rather supports psychiatric care.</div>", unsafe_allow_html=True)
+# if __name__ == '__main__':
+#     pass
